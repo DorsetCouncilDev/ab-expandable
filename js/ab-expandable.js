4fcbede7974a0f1c44a1fb7d4910da12fb049cf7
Adam Burton <a.burton@dorsetcc.gov.uk>
2019-12-27 14:08:59 +0000

    test

--- js/ab-expandable.js
+++ js/ab-expandable.js
@@ -3,7 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
     for(var i=0; i<expandables.length;i++){
          expandables[i].addEventListener("click", function(evt) {
          var btn = evt.target;
-         var content = btn.nextSibling;
+         var content = btn.nextElementSibling;
+         console.log(content)
            if(btn.getAttribute("aria-expanded") == "true"){
                btn.setAttribute("aria-expanded",false);
                content.style.display = "none";
