61186c2247a6f249071078eede82e6393028716a
Adam Burton <a.burton@dorsetcc.gov.uk>
2019-12-27 14:17:28 +0000

    removed console log

--- js/ab-expandable.js
+++ js/ab-expandable.js
@@ -1,18 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     const expandables = document.querySelectorAll(".ab-exapndable-button");
     for(var i=0; i<expandables.length;i++){
-         expandables[i].addEventListener("click", function(evt) {
-         var btn = evt.target;
-         var content = btn.nextElementSibling;
-         console.log(content)
-           if(btn.getAttribute("aria-expanded") == "true"){
-               btn.setAttribute("aria-expanded",false);
-               content.style.display = "none";
-           }
-           else{
-               btn.setAttribute("aria-expanded",true);
-               content.style.display = "block"; 
-           }
+        expandables[i].addEventListener("click", function(evt) {
+            var btn = evt.target;
+            var content = btn.nextElementSibling;
+            if(btn.getAttribute("aria-expanded") == "true"){
+                btn.setAttribute("aria-expanded",false);
+                content.style.display = "none";
+            }
+            else{
+                btn.setAttribute("aria-expanded",true);
+                content.style.display = "block"; 
+            }
         })
     }
- })
+})
